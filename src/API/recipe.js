37e7c31a477edd1e00ad18cf92ef16bea51d9044
Sarahalhbaish <sarah21995@live.com>
2024-12-19 18:30:15 +0300

    Recipes Done

--- src/API/recipe.js
+++ src/API/recipe.js
@@ -22,17 +22,11 @@ const createRecipe = async (name, ingredients, instructions, image) => {
 };
 const updateRecipe = async (
   recipeId,
-  name,
-  ingredients,
-  instructions,
-  image
+  { name, ingredients, instructions, image }
 ) => {
-  const response = await fetch(`/recipes/${recipeId}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name, ingredients, instructions, image }),
+  const response = await instance.put(`/recipes/${recipeId}`, {
+   
+    name, ingredients, instructions, image,
   });
   if (!response.ok) {
     throw new Error("Error updating recipe");
